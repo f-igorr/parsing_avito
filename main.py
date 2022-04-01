b6b7102eb9729756a08812d42af6da6fdc8fafa0
f-igorr <f_igorr@mail.ru>
2022-04-01 14:21:37 +0300

    need off PC

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from ast import Raise
 import requests
 from bs4 import BeautifulSoup, SoupStrainer
 import lxml
@@ -49,8 +50,9 @@ def request_with_check_200(url, dict_sleep, text):
     status_200 = False
     count = 1 # счетчик попыток
     while not status_200: # пока status_200 = False посылаем запросы
-        num_random_header = random.randint(0,len(HEADERS)-1) # рандомный индекс для списка хедеров
-        response = requests.get(url=url, headers=HEADERS[num_random_header], timeout= TIMEOUT)
+        num_random_header = 0 #random.randint(0,len(HEADERS)-1) # рандомный индекс для списка хедеров
+        rand_header = HEADERS[num_random_header]
+        response = requests.get(url=url, headers=rand_header, timeout= TIMEOUT)
         if response.status_code == 200:
             print(f'{text}. попытка {count} OK')
             src = response.text
@@ -59,19 +61,20 @@ def request_with_check_200(url, dict_sleep, text):
             print(f'{text}. попытка {count} BAD')
             time.sleep(random.gauss(mu=dict_sleep['mu'] * count, sigma=dict_sleep['sigma']))
             count += 1
+        if count > 5:
+            break
     
-    return src
+    return src, rand_header
 
 
 
-def find_last_page(url):
+def find_last_page(url, dict_sleep):
     ''' смотрим сколько страниц перебирать 
     url: страница нач запроса
     '''
     print('Start <find_last_page> ...')
-    dict_sleep = {'mu': 2.0, 'sigma': 1.0}
     text = 'поиск номера последней страницы'
-    src = request_with_check_200(url, dict_sleep, text)
+    src, rand_header = request_with_check_200(url, dict_sleep, text)
     filter = SoupStrainer('div') #, class_ = re.compile('pagination-root')) #будем готовить суп только из 'div'
     soup = BeautifulSoup(src, parser, parse_only=filter)
     last_page = soup.find_all(attrs={f'data-marker': {re.compile('page')}})[-1].text.strip()
@@ -80,38 +83,17 @@ def find_last_page(url):
     return int(last_page) # номер послед страницы
 
 
-#def search_links_from_page(url):
-#    ''' собираем ссылки на обявл с конкретной страницы 
-#    url: ссылка с номером страницы
-#    '''
-#    print('Start search_links_from_page...')
-#    dict_sleep = {'mu': 2.0, 'sigma': 1.0}
-#    text = 'загрузка страницы с объявлениями'
-#    src = request_with_check_200(url, dict_sleep, text)
-#
-#    #filter = SoupStrainer('a') #будем готовить суп только из ссылок
-#    soup = BeautifulSoup(src, parser) #, parse_only=filter)
-#    tags_links = soup.find_all('a', class_=re.compile('link-link'), href = re.compile(LOCATION_FILTER), attrs = {'data-marker': 'item-title'})
-#    links = []
-#    for tag in tags_links:
-#        link = tag.get('href')
-#        links.append(link)
-#
-#    return links
-
-
-def collect_all_hrefs(url, last_page):
+def collect_all_hrefs(url, dict_sleep, last_page):
     ''' перебор всех страниц 
     url: ссылка без номера страницы
     last_pages: номер последней страницы
     '''
     all_hrefs = []
-    dict_sleep = {'mu': 2.0, 'sigma': 1.0}
     for page in range(1, last_page + 1): # перебор страниц
         url = f'{url}&p={str(page)}'
         print(f'Start <collect_all_hrefs> from page {page}...')
         text = f'загрузка страницы {page}'
-        src = request_with_check_200(url, dict_sleep, text)
+        src, rand_header = request_with_check_200(url, dict_sleep, text)
         filter = SoupStrainer('a') #будем готовить суп только из ссылок
         soup = BeautifulSoup(src, parser, parse_only=filter)
         tags_links = soup.find_all(class_= re.compile('link-link'), href = re.compile(LOCATION_FILTER), attrs = {'data-marker': 'item-title'})
@@ -125,109 +107,96 @@ def collect_all_hrefs(url, last_page):
     return all_hrefs
 
 
-def collect_all_data(hrefs):
+def collect_all_data(hrefs, dict_sleep):
     ''' собираем данные из одного объявления 
     hrefs: список ссылок на объявления
     '''
     all_data = []
-    dict_sleep = {'mu': 2.0, 'sigma': 1.0}
-    for i, href in enumerate(hrefs):
+    for i, href in enumerate(hrefs, start=1):
         print(f'collecting data url {i} / {len(hrefs)}')
         url = PREFIX + href
         text = f'загрузка объявления'
-        src = request_with_check_200(url, dict_sleep, text)
+        src, rand_header = request_with_check_200(url, dict_sleep, text)
         dict_data = {}
         soup = BeautifulSoup(src, parser)
 
-        try:
-            title       = soup.find('span', class_ = re.compile('title-info-title-text')).text.strip()
-        except Exception:
-            title = 'not found'
+        #flag_not_found = True
+        count = 1
+        while count < 5: #flag_not_found == True:
+
+            try:
+                title       = soup.find('span', class_ = re.compile('title-info-title-text')).text.strip()
+            except Exception:
+                title = 'not found'
+
+            try:        
+                price       = soup.find('span', class_ = re.compile('item-price'), attrs = {'itemprop': 'price'})['content']
+            except Exception:
+                price = 'not found'
+
+            try:
+                zalog       = soup.find('div', text = re.compile('залог')).text.strip().replace(u'\xa0', u' ')
+            except Exception:
+                zalog = 'not found'    
+
+            try:
+                animals     = soup.find('span', text = re.compile('Можно с животными')).next_sibling.strip()
+            except Exception:
+                animals = 'not found'  
+                with open(file='soup.txt', mode='w') as file:
+                    file.write(soup.text)
+                    raise
+
+            try:    
+                address     = soup.find('span', class_ = re.compile('item-address')).text.strip()
+            except Exception:
+                address = 'not found'   
+
+            try:    
+                description = soup.find('div', class_= re.compile('item-description'), itemprop='description').text.strip()
+            except Exception:
+                description = 'not found'    
+
+            try:    
+                seller      = soup.find(attrs = {'data-marker': 'seller-info/label'}).text.strip()
+            except Exception:
+                seller = 'not found'   
+
+            try:    
+                date        = soup.find('div', class_= re.compile('title-info-metadata-item-redesign')).text.strip()
+            except Exception:
+                date = 'not found'
+
+            try:
+                technics    = soup.find('span', text = re.compile('Техника:')).next_sibling.strip()
+            except Exception:
+                technics = 'not found'
+
+            dict_data['title']       = title
+            dict_data['price']       = price
+            dict_data['zalog']       = zalog
+            dict_data['animals']     = animals
+            dict_data['condition']   = 'да' if re.findall(r'[Кк]ондиционер', description + technics) else 'нет'
+            dict_data['seller']      = seller
+            dict_data['date']        = date
+            dict_data['address']     = address
+            dict_data['technics']    = technics
+            dict_data['description'] = description
+            dict_data['url']         = url
+            dict_data['rand_header'] = rand_header
+
+            if 'not found' in dict_data.values():
+                #flag_not_found = True
+                print(f'not_found. trying again {count}...')
+                time.sleep(random.gauss(mu=dict_sleep['mu'] * count, sigma=dict_sleep['sigma']))
+                count += 1
+            else:
+                #flag_not_found = False
+                break
+                
+        all_data.append(dict_data)
         
-        try:        
-            price       = soup.find('span', class_ = re.compile('item-price'), attrs = {'itemprop': 'price'})['content']
-        except Exception:
-            price = 'not found'
-        
-        try:
-            zalog       = soup.find('div', text = re.compile('залог')).text.strip().replace(u'\xa0', u' ')
-        except Exception:
-            zalog = 'not found'    
-            
-        try:
-            animals     = soup.find('span', text = re.compile('Можно с животными')).next_sibling.strip()
-        except Exception:
-            animals = 'not found'    
-            
-        try:    
-            address     = soup.find('span', class_ = re.compile('item-address')).text.strip()
-        except Exception:
-            address = 'not found'    
-            
-        try:    
-            description = soup.find('div', class_= re.compile('item-description-text'), itemprop='description').text.strip()
-        except Exception:
-            description = 'not found'    
-
-        try:    
-            seller      = soup.find(attrs = {'data-marker': 'seller-info/label'}).text.strip()
-        except Exception:
-            seller = 'not found'    
-            
-        try:    
-            date        = soup.find('div', class_= re.compile('title-info-metadata-item-redesign')).text.strip()
-        except Exception:
-            date = 'not found'
-
-        try:
-            technics = soup.find('li', class_= re.compile('params-paramsList')).text.strip()
-        except Exception:
-            technics = 'not found'
-
-
-
-
-
-def collect_data_2(url):
-    ''' собираем данные из одного объявления 
-    url: ссылка на конкретное объявление
-    '''
-    num_random_header = random.randint(0,len(HEADERS)-1) # рандомный индекс для списка хедеров
-    response = requests.get(url=url, headers=HEADERS[num_random_header], timeout= TIMEOUT)
-    if response.status_code == 200:
-        src = response.text
-    else:
-        return print('Bad status_code')
-    res = {}
-    soup = BeautifulSoup(src, parser) #, parse_only=filter)
-    title       = soup.find('span', class_ = re.compile('title-info-title-text')).text.strip()
-    price       = soup.find('span', class_ = re.compile('item-price'), attrs = {'itemprop': 'price'})['content']
-    zalog       = soup.find('div', text = re.compile('залог')).text.strip().replace(u'\xa0', u' ')
-    try:
-        animals     = soup.find('span', text = re.compile('Можно с животными')).next_sibling.strip()
-        with open(file='link_OK.html', mode='w') as file:
-            file.write(response.text)
-    except:
-        with open(file='link_BAD.html', mode='w') as file:
-            file.write(response.text)
-    #animals     = soup.find('span', class_ = re.compile('item-params-label'), text = re.compile('Можно с животными')).next_sibling.strip()
-    address     = soup.find('span', class_ = re.compile('item-address')).text.strip()
-    description = soup.find('div', class_= re.compile('item-description-text'), itemprop='description').text.strip()
-    seller      = soup.find(attrs = {'data-marker': 'seller-info/label'}).text.strip()
-    date        = soup.find('div', class_= re.compile('title-info-metadata-item-redesign')).text.strip()
-    
-    res['title']       = title
-    res['price']       = price
-    res['zalog']       = zalog
-    res['animals']     = animals
-    res['condition']   = 'да' if re.findall(r'[Кк]ондиционер', description) else 'нет'
-    res['seller']      = seller
-    res['date']        = date
-    res['address']     = address
-    res['description'] = description
-    res['url']         = url
-
-    return res
+    return all_data
 
 
 def write_to_csv(result, filename='results.csv'):
@@ -245,25 +214,19 @@ def write_to_csv(result, filename='results.csv'):
 
 def main():
     
-    last_page = find_last_page(URL_WO_PAGE)
+    dict_sleep = {'mu': 10.0, 'sigma': 1.0}
+    last_page = find_last_page(URL_WO_PAGE, dict_sleep)
 
+    dict_sleep = {'mu': 15.0, 'sigma': 2.0}
     while True:
-        all_hrefs = collect_all_hrefs(URL_WO_PAGE, last_page)
-        all_hrefs_2 = collect_all_hrefs(URL_WO_PAGE, last_page)
+        all_hrefs = collect_all_hrefs(URL_WO_PAGE, dict_sleep, last_page)
+        all_hrefs_2 = collect_all_hrefs(URL_WO_PAGE, dict_sleep, last_page)
         if len(all_hrefs) == len(all_hrefs_2):
             break
     print('End collect_all_links')
     
-
-
-    results = []
-    count_links = 1
-    for link in all_links:
-        url = PREFIX + link
-        print(f'Start collect_data {count_links} from {len(all_links)} url={url} ...')
-        results.append(collect_data(url))
-        time.sleep(random.gauss(mu=TIME_SLEEP['mu'], sigma=TIME_SLEEP['sigma']))
-        count_links += 1
+    dict_sleep = {'mu': 30.0, 'sigma': 5.0}
+    results = collect_all_data(all_hrefs, dict_sleep)
 
     write_to_csv(results)
 
--- save_page.py
+++ save_page.py
@@ -1,4 +1,6 @@
 import requests
+from bs4 import BeautifulSoup
+import re
 
 
 
@@ -8,14 +10,37 @@ HEADERS = {
     'Accept': 'image/avif,image/webp,*/*'
 
 }
-URL_1 = 'https://www.avito.ru/krasnodar/doma_dachi_kottedzhi/dom_34_m_na_uchastke_3_sot._2363871707'
+URL_1 = 'https://www.avito.ru/krasnodar/doma_dachi_kottedzhi/dom_60_m_na_uchastke_4_sot._2352410171'
 #URL_2 = 'https://www.avito.ru/krasnodar/doma_dachi_kottedzhi/dom_40_m_na_uchastke_1_sot._2276656851'
 
 response = requests.get(url=URL_1, headers=HEADERS)
-print(response.content)
 with open(file='link_1.html', mode='w') as file:
-   file.write(response) #.text)
+   file.write(response.text)
 
 #response = requests.get(url=URL_2, headers=HEADERS)
 #with open(file='link_2.html', mode='w') as file:
 #   file.write(response.text)
+
+with open(file='link_1.html') as file:
+    src = file.read()
+
+soup = BeautifulSoup(src, 'lxml')
+try:
+    #technics    = soup.find_all('li', class_= 'item-params-list-item').find('span', text = re.compile('Техника:')).text.strip()
+    technics    = soup.find('span', text = re.compile('Техника:')).next_sibling.strip()
+except Exception:
+    technics = 'not found'
+
+try:    
+    description = soup.find('div', class_= re.compile('item-description'), itemprop='description').text.strip()
+except Exception:
+    description = 'not found'
+
+try:
+    animals     = soup.find('span', text = re.compile('Можно с животными')).next_sibling.strip()
+except Exception:
+    animals = 'not found'  
+
+print('technics = ', technics)
+print('description = ', description)
+print('animals = ', animals)
